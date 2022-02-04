3e4d1b958d9bb180708ce2176edcd21b913944f6
elf3 <elf3@google.com>
2022-02-04 16:23:39 +0800

    INIT DEMO

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::detect::__is_feature_detected::fma;
+use poem::{
+    get, handler, IntoResponse,
+    listener::TcpListener, Route, Server, web::Path,
+};
+use tokio::{
+    main as tokio_main
+};
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+#[tokio_main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/hello/:name", get(hello));
+    Server::new(TcpListener::bind("127.0.0.1:3000")).run(app).await
 }
